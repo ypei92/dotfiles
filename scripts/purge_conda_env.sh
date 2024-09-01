847838fa5890ba2ef1d0e9f140eafd644ba21a26
ypei <ypei@utexas.edu>
2024-09-01 15:28:10 -0700

    Update purge_conda_env.sh

--- scripts/purge_conda_env.sh
+++ scripts/purge_conda_env.sh
@@ -1,1 +1,11 @@
-(TODO) placeholder
+#!/bin/bash
+
+env_name=$CONDA_DEFAULT_ENV
+default_python_version=3.10
+python_version="${1:-$default_python_version}"
+
+# deactivate, delete, re-create and activate
+conda deactivate
+conda remove -n $env_name -y --all
+conda create -n $env_name -y python=$python_version
+conda activate $env_name
