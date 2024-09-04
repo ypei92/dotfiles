40db9a4c73fc76708e22ff715ce5368705233f4d
ypei <ypei@utexas.edu>
2024-09-03 22:51:31 -0700

    add docker installation script

---
+++ scripts/debian/debian_docker_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Dependencies
+sudo apt update
+sudo apt install ca-certificates curl
+
+# GPG Setup keyring for Docker
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add repo to apt + installation
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add user to docker group to avoid using sudo
+sudo usermod -aG docker ypei
